6f732de4723c4cc4461f623726776ee5b0bbe531
Esteffy Bachiller <esteffy.bc@gmail.com>
2025-05-26 13:24:54 -0500

    Ultimos cambios

--- resources/views/errors/role_deactivated.blade.php
+++ resources/views/errors/role_deactivated.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container py-5">
-    <div class="alert alert-danger text-center">
-      <h2>Su rol ha sido desactivado.</h2>
-      <p>Por favor, contacte al administrador.</p>
+  <div class="role-deactivated">
+    <div class="container py-5">
+      <div class="alert alert-danger text-center">
+        <h2>Su rol ha sido desactivado.</h2>
+        <p>Por favor, contacte al administrador.</p>
+      </div>
     </div>
   </div>
 @endsection
